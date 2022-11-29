2195f595678aa23e58a305a5776577c9449a4c17
Liangyu-Zhou <17816872833@163.com>
2022-11-29 10:39:55 +0800

    first demo commit, remove angular

--- web/ng/src/environments/token.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export interface IEnvironment {
-    production: boolean;
-    apiEndpoint: string;
-    catpchaSiteKey: string;
-}
-
-export const ENVIRONMENT = new InjectionToken<IEnvironment>('ENVIRONMENT');
